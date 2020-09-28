e1f1f42824b1b7fdb72a3bcfa0429c70b021b523
Begotten Son <BegottenSounds@DANIELs-MacBook-Pro.local>
2020-09-28 19:36:49 -0400

    set kinetic toggle

--- src/components/ToggleFriction.svelte
+++ src/components/ToggleFriction.svelte
@@ -1,13 +1,5 @@
 <script>
-    import { fly } from 'svelte/transition';
-    import { cubicIn, elasticIn } from 'svelte/easing';
-    export let name = " Toggle Friction";
-
     let checked = false;
-
-    function toggle() {
-        checked = !checked;
-    }
 </script>
 
 <style>
@@ -47,11 +39,11 @@
         width: 2.3em;
         height: 2.3em;
         top: -0.1em;
-        left: -5em;
+        transform: translateX(-5em);
         border-radius: 2em;
         border: 2px solid var(--gum);
         background-color: var(--gum);
-        transition: 300ms cubic-bezier(0.95, -0.02, 0.71, 1);
+        transition: 300ms 280ms cubic-bezier(0,.42,.5,1.04);
     }
 
     #balls:after {
@@ -70,35 +62,70 @@
 
     #leftString {
         position: absolute;
-        /* transform: translateX(-3.6em); */
         top: -2.9em;
         width: 0.12em;
-        height: 3em;
+        height: 4em;
         background-color: rgb(229, 198, 240);
+    }
+
+    #rightString {
+        position: absolute;
+        top: -2.9em;
+        width: 0.12em;
+        height: 4em;
+        background-color: rgb(229, 198, 240);
+        
+    }
+
+    #centerString {
+        position: absolute;
+        top: -2.9em;
+        left: 1.2em;
+        width: 0.12em;
+        height: 4em;
+        background-color: rgb(229, 198, 240);
+    }
+
+    .left-string-original {
+        transform: translateX(-3em) rotate(20deg);
+        transition: 300ms 280ms cubic-bezier(0,.42,.5,1.04);
+    }
+    .left-string-move {
+        transform: translateX(-1.3em);
         transition: 300ms cubic-bezier(0.95, -0.02, 0.71, 1);
     }
 
+    .right-string-original {
+        transform: translateX(3.8em);
+        transition: 300ms cubic-bezier(0.95, -0.02, 0.71, 1);
+    }
+    .right-string-move {
+        transform: translateX(4.8em) rotate(-20deg);
+        transition: 300ms 280ms cubic-bezier(0,.42,.5,1.04);
+    }
+
     #toggleFriction:checked + #balls:before {
-        left: -2.7em;
+        transform: translateX(-2.7em);
+        transition: 300ms cubic-bezier(0.95, -0.02, 0.71, 1);
     }
 
     #toggleFriction:checked + #balls:after {
-        left: 5em;
+        transform: translateX(4.5em);
+        transition: 300ms 280ms cubic-bezier(0,.42,.5,1.04);
     }
 
-    #toggleFriction:checked + #balls {
-    }
+    
+
 </style>
 
 <section>
-    <h1> {name} </h1>
+    <h1> Kinetic Toggle </h1>
     <div class="toggle">
-        {#if checked}
-        <div id="leftString" transition:fly="{{x: 100, easing: elasticIn}}"></div>   
-        {/if}
-        
-        <label for="toggleFriction" id="balls">
-         <input type="checkbox" bind:checked id="toggleFriction">   
-        </label>
+        <div id="leftString" class="{checked ? 'left-string-move' : 'left-string-original'}"></div>
+        <div id="rightString" class="{checked ? 'right-string-move' : 'right-string-original'}"></div>
+        <div id="centerString"></div>
+         <input type="checkbox" bind:checked id="toggleFriction"> 
+         <label for="toggleFriction" id="balls"></label> 
+          
     </div>
 </section>
